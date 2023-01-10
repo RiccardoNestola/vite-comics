3995203d66fa29554180e03e07a7dec8ec9732e5
RiccardoNestola <riccardinux@gmail.com>
2023-01-10 16:59:33 +0100

    add product component

---
+++ src/components/ProductComp.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+
+    props:{
+        img: String,
+        price: Number,
+        series: String,
+        type:String
+    }
+    
+}
+</script>
+
+<template>
+    <div>
+        <img :src="img" :alt="type">
+        <h2>{{ series }}</h2>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
